481cabdebc8b8d032f7c2100033462fc18939156
Kristijan_Papic <kristijanpapic04@gmail.com>
2022-02-04 12:07:36 +0100

    added datetimemodel

---
+++ AuTOP/AuTOP.Model.Common/DateTimeModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuTOP.Model.Common
+{
+    public abstract class DateTimeModel
+    {
+       private DateTime dateCreated;
+       private DateTime dateUpdated;
+
+        public DateTime DateCreated { get => dateCreated; set => dateCreated = value; }
+        public DateTime DateUpdated { get => dateUpdated; set => dateUpdated = value; }
+    }
+}
