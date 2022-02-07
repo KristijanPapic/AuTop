23e67010baf0a3458fb288dc979225e4470af5c0
Marko <marko.dankovic2541@gmail.com>
2022-02-07 15:41:46 +0100

    add IModelVersionRepository

---
+++ AuTOP/AuTOP.Repository.Common/IModelVersionRepository.cs
@@ -0,0 +1,17 @@
+﻿using AuTOP.Common;
+using AuTOP.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuTOP.Repository.Common
+{
+    public interface IModelVersionRepository 
+    {
+       Task<List<ModelVersion>> GetAllModelVersions(Sorting sort, Paging paging);
+
+       Task<ModelVersion> GetModelVersionById(string name);
+    }
+}
